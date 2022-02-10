905978143724045598890cac55a0aa654d2a8fab
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-10 09:12:41 +0200

    add materials

---
+++ js/timer.js
@@ -0,0 +1,12 @@
+/*
+ * Реализовать таймер, показывающий сколько времени прошло с дня рождения юзера
+ * `${days} days / ${hours} hours / ${mins} minutes / ${secs} seconds`
+ * 10562 days / 05 hours / 30 minutes / 10 seconds
+ * Часы, минуты и секунды должны быть с нулями
+ */
+
+// const timerRef = document.querySelector('.timer');
+// const birthDateRef = document.querySelector('.birthday');
+
+// const userBirthDate = '2000-02-28';
+// birthDateRef.textContent = userBirthDate;
